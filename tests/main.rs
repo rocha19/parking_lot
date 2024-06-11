908771517f67e4b4356be9fc87bf9aa227a7a6af
rocha19 <rochaarqueologia@outlook.com>
2024-06-11 09:27:40 -0300

    test: closed and full parking_lot

--- tests/main.rs
+++ tests/main.rs
@@ -38,23 +38,18 @@ mod tests {
 
     #[tokio::test]
     async fn closed_parking_lot() {
+        use chrono::NaiveDateTime;
+
         let parking_lot_repository_in_memory = ParkingLotRepositoryInMemory::new();
         let repository: Arc<RwLock<dyn ParkingLotRepository>> =
             Arc::new(RwLock::new(parking_lot_repository_in_memory));
-        let enter_parking_lot = EnterParkingLot::new(repository.clone());
-        let get_parking_lot = GetParkingLot::new(repository);
+        let get_parking_lot = GetParkingLot::new(repository.clone());
 
-        let parking_lot_before_enter = get_parking_lot.execute("shopping_mall".to_string()).await;
-        assert_eq!(parking_lot_before_enter.occupied_spaces, 0);
+        let parking_lot = get_parking_lot.execute("shopping_mall".to_string()).await;
 
-        let date =
+        let current_time =
             NaiveDateTime::parse_from_str("2022-01-01T23:00:00", "%Y-%m-%dT%H:%M:%S").unwrap();
-        let _ = enter_parking_lot
-            .execute("shopping_mall".to_string(), "ABC1234".to_string(), date)
-            .await;
-
-        let parking_lot_after_enter = get_parking_lot.execute("shopping_mall".to_string()).await;
-        assert_eq!(parking_lot_after_enter.occupied_spaces, 1);
+        assert!(!parking_lot.is_open(current_time));
     }
 
     #[tokio::test]
@@ -63,49 +58,44 @@ mod tests {
         let repository: Arc<RwLock<dyn ParkingLotRepository>> =
             Arc::new(RwLock::new(parking_lot_repository_in_memory));
         let enter_parking_lot = EnterParkingLot::new(repository.clone());
-        let get_parking_lot = GetParkingLot::new(repository);
 
-        let parking_lot_before_enter = get_parking_lot.execute("shopping_mall".to_string()).await;
-        assert_eq!(parking_lot_before_enter.occupied_spaces, 0);
-
-        let date =
-            NaiveDateTime::parse_from_str("2022-01-01T10:00:00", "%Y-%m-%dT%H:%M:%S").unwrap();
-
-        for i in 0..10 {
-            let plate_number = format!("ABC123{}", i);
+        for _ in 0..10 {
+            let date =
+                NaiveDateTime::parse_from_str("2022-01-01T9:00:00", "%Y-%m-%dT%H:%M:%S").unwrap();
             let _ = enter_parking_lot
-                .execute("shopping_mall".to_string(), plate_number, date)
+                .execute("shopping_mall".to_string(), "ABC1234".to_string(), date)
                 .await;
         }
 
-        let parking_lot_after_enter = get_parking_lot.execute("shopping_mall".to_string()).await;
-        assert_eq!(parking_lot_after_enter.occupied_spaces, 1);
+        let date =
+            NaiveDateTime::parse_from_str("2022-01-01T9:00:00", "%Y-%m-%dT%H:%M:%S").unwrap();
+        let result = enter_parking_lot
+            .execute("shopping_mall".to_string(), "XYZ5678".to_string(), date)
+            .await;
+        assert!(result.is_err());
     }
 
     #[tokio::test]
     async fn get_parking_lot() {
-        // Criação da tabela parking_lots antes de executar o teste
-        let connection = ParkingLotRepositorySqlite::new("./parking_lot.sqlite")
-            .establish_connection()
-            .expect("Failed to establish connection");
-        connection
-            .execute(
-                "CREATE TABLE IF NOT EXISTS parking_lots (
-                code TEXT PRIMARY KEY,
-                capacity INTEGER,
-                open_hour INTEGER,
-                close_hour INTEGER,
-                occupied_spaces INTEGER
-            )",
-                [],
-            )
-            .expect("Failed to create table");
+        // let connection = ParkingLotRepositorySqlite::new("./parking_lot.sqlite")
+        //     .establish_connection()
+        //     .expect("Failed to establish connection");
+        // connection
+        //     .execute(
+        //         "CREATE TABLE IF NOT EXISTS parking_lots (
+        //         code TEXT PRIMARY KEY,
+        //         capacity INTEGER,
+        //         open_hour INTEGER,
+        //         close_hour INTEGER,
+        //         occupied_spaces INTEGER
+        //     )",
+        //         [],
+        //     )
+        //     .expect("Failed to create table");
 
-        // Adição do estacionamento
         let parking_lot_repository_sqlite = ParkingLotRepositorySqlite::new("./parking_lot.sqlite");
         parking_lot_repository_sqlite.add_parking_lot();
 
-        // Execução do teste
         let repository: Arc<RwLock<dyn ParkingLotRepository>> =
             Arc::new(RwLock::new(parking_lot_repository_sqlite));
         let get_parking_lot = GetParkingLot::new(repository);
